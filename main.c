23000587edb6e1a4da4ef252b22fd0a26b534022
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-09 15:20:25 +0900

    Use ngx_log_init_name added to libnginx

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <nginx/ngx_core.h>
 
-ngx_uint_t             ngx_test_config;
-
-extern ngx_int_t ngx_save_argv(ngx_cycle_t *cycle, int argc, char *const *argv);
-
-
 int main(int argc, char **argv)
 {
-    ngx_cycle_t *cycle, init_cycle;
     ngx_log_t   *log;
   
     ngx_debug_init();
@@ -18,51 +12,18 @@ int main(int argc, char **argv)
   
     ngx_time_init();
 
-    log = ngx_log_init((u_char *) ".");
+    log = ngx_log_init_name((u_char *) "debug.log");
     if (log == NULL) {
         return 1;
     }
     log->log_level = NGX_LOG_DEBUG;
     ngx_use_stderr = 0;
-  
-    ngx_memzero(&init_cycle, sizeof(ngx_cycle_t));
-    init_cycle.log = log;
-    cycle = &init_cycle;
-  
-    if (ngx_save_argv(&init_cycle, argc, argv) != NGX_OK) {
-        return 1;
-    }
-
-    init_cycle.pool = ngx_create_pool(1024, log);
-    if (init_cycle.pool == NULL) {
-        return 1;
-    }
-
-    if (ngx_os_init(log) != NGX_OK) {
-        return 1;
-    }
-
-    ngx_slab_sizes_init();
-
-    if (ngx_preinit_modules() != NGX_OK) {
-        return 1;
-    }
-
-    cycle->conf_file.len = strlen("nginx.conf");
-    cycle->conf_file.data = (u_char *)"nginx.conf";
-
-    cycle = ngx_init_cycle(&init_cycle);
-    if (cycle == NULL) {
-        if (ngx_test_config) {
-            ngx_log_stderr(0, "configuration file %s test failed",
-                           init_cycle.conf_file.data);
-        }
-
-        return 1;
-    }
 
     ngx_log_error(NGX_LOG_ERR, log, 0, "hello nginx log %d", 1);
     ngx_log_error(NGX_LOG_ERR, log, 2, "some error %s", "badarg");
+
+    sleep(1);
+    ngx_time_update();
     ngx_log_debug0(NGX_LOG_DEBUG, log, 0, "This is debug a log");
     ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "This is debug a log, a=%d", 1);
     return 0;
