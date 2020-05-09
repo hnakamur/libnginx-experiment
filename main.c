326e5db682712f82a7b25915aa19e14542ea91ea
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-09 15:33:00 +0900

    Use memory pool

--- main.c
+++ main.c
@@ -2,17 +2,13 @@
 
 int main(int argc, char **argv)
 {
-    ngx_log_t   *log;
-  
     ngx_debug_init();
-  
     if (ngx_strerror_init() != NGX_OK) {
         return 1;
     }
-  
     ngx_time_init();
 
-    log = ngx_log_init_name((u_char *) "debug.log");
+    ngx_log_t *log = ngx_log_init_name((u_char *) "debug.log");
     if (log == NULL) {
         return 1;
     }
@@ -26,5 +22,14 @@ int main(int argc, char **argv)
     ngx_time_update();
     ngx_log_debug0(NGX_LOG_DEBUG, log, 0, "This is debug a log");
     ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "This is debug a log, a=%d", 1);
+
+    ngx_pool_t *pool = ngx_create_pool(128, log);
+    void *data = ngx_pcalloc(pool, 128);
+    ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "data=%p", data);
+    int ret = ngx_pfree(pool, data);
+    ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "ngx_pfree ret=%d", ret);
+
+    ngx_destroy_pool(pool);
+
     return 0;
 }
