ab03851c338227f9e89de702deb0d3814271dca7
Hiroaki Nakamura <hnakamur@gmail.com>
2020-05-09 18:10:46 +0900

    Seperate libnginx initialization to function

--- main.c
+++ main.c
@@ -1,19 +1,39 @@
 #include <nginx/ngx_core.h>
 
-int main(int argc, char **argv)
+int libnginx_init(ngx_log_t **log, const char *log_filename, unsigned log_level, ngx_uint_t use_stderr)
 {
     ngx_debug_init();
-    if (ngx_strerror_init() != NGX_OK) {
-        return 1;
+    int ret = ngx_strerror_init();
+    if (ret != NGX_OK) {
+        return ret;
     }
     ngx_time_init();
 
-    ngx_log_t *log = ngx_log_init_name((u_char *) "debug.log");
-    if (log == NULL) {
-        return 1;
+    ngx_pid = ngx_getpid();
+    ngx_parent = ngx_getppid();
+
+    ngx_log_t *l = *log = ngx_log_init_name((u_char *) log_filename);
+    l->log_level = log_level;
+    ngx_use_stderr = use_stderr;
+
+    ngx_cycle_t init_cycle;
+    ngx_memzero(&init_cycle, sizeof(ngx_cycle_t));
+    init_cycle.log = l;
+    ngx_cycle = &init_cycle;
+
+    ret = ngx_os_init(l);
+    if (ret != NGX_OK) {
+        return ret;
     }
-    log->log_level = NGX_LOG_DEBUG;
-    ngx_use_stderr = 0;
+    ngx_slab_sizes_init();
+
+    return NGX_OK;
+}
+
+int main(int argc, char **argv)
+{
+    ngx_log_t *log;
+    int ret = libnginx_init(&log, "debug.log", NGX_LOG_DEBUG, 0);
 
     ngx_log_error(NGX_LOG_ERR, log, 0, "hello nginx log %d", 1);
     ngx_log_error(NGX_LOG_ERR, log, 2, "some error %s", "badarg");
@@ -23,13 +43,38 @@ int main(int argc, char **argv)
     ngx_log_debug0(NGX_LOG_DEBUG, log, 0, "This is debug a log");
     ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "This is debug a log, a=%d", 1);
 
-    ngx_pool_t *pool = ngx_create_pool(128, log);
-    void *data = ngx_pcalloc(pool, 128);
+    /* ==============================
+     * memory pool usage example
+     * ============================== */
+    /* NOTE: The minimum size is 80.
+     * With size <= 79, ngx_pfree returns -NGX_DECLINE. */
+    ngx_pool_t *pool = ngx_create_pool(80, log);
+    void *data = ngx_pcalloc(pool, 80);
     ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "data=%p", data);
-    int ret = ngx_pfree(pool, data);
+    ret = ngx_pfree(pool, data);
     ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "ngx_pfree ret=%d", ret);
-
     ngx_destroy_pool(pool);
+
+    /* ==============================
+     * slab pool usage example
+     * ============================== */
+    const size_t pool_size = 1024 * 1024; /* 1MiB */
+    /* NOTE: A shared memory is used in nginx for backstore of slab_pool,
+     * but we use an unshared memory here since we use only one process. */
+    ngx_slab_pool_t *sp = malloc(pool_size);
+    if (sp == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        return 1;
+    }
+    if (ngx_slab_init_size(sp, pool_size) != NGX_OK) {
+        fprintf(stderr, "ngx_slab_init_size failed\n");
+        return NGX_ERROR;
+    }
+
+    void *p = ngx_slab_calloc(sp, 20);
+    ngx_log_debug1(NGX_LOG_DEBUG, log, 0, "slab_alloc p=%p", p);
+    ngx_slab_free(sp, p);
+    free(sp);
 
     return 0;
 }
